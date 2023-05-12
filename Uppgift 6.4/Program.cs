6100495f0b0c906d8aa241f3d501b9b265d5b281
Admin <Admin@TEB123008>
2023-05-12 09:22:25 +0200

    Add project files.

---
+++ Uppgift 6.4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class program
+{
+    static int potens(int tal1, int tal2)
+    {
+        return (int)Math.Pow(tal1, tal2);
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine(potens(4, 2));
+    }
+}
